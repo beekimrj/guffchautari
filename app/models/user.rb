4faf7b10895296498c1b9958b4132c2d970ab426
beekimrj <beekimrj@yahoo.com>
2020-05-20 16:36:21 +0545

    Login with email or username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+	attr_accessor :login
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,4 +10,10 @@ class User < ApplicationRecord
   has_many :chatroom_users
   has_many :chatroom, through: :chatroom_users
   has_many :messages
+
+	def self.find_for_database_authentication warden_conditions
+	  conditions = warden_conditions.dup
+	  login = conditions.delete(:login)
+	  where(conditions).where(["lower(username) = :value OR lower(email) = :value", {value: login.strip.downcase}]).first
+	end
 end
