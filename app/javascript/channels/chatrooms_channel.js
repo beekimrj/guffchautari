ded9fc6b1616f0d70408fb9d792e072a35a5b00a
beekimrj <beekimrj@yahoo.com>
2020-05-24 00:22:32 +0545

    use websocket to receive and send message, i.e. messagecontroller is not need to create msg

--- app/javascript/channels/chatrooms_channel.js
+++ app/javascript/channels/chatrooms_channel.js
@@ -17,5 +17,21 @@ consumer.subscriptions.create("ChatroomsChannel", {
     }else{
     $(`[data-behavior='chatroom-link'][data-chatroom-id='${data.chatroom_id}']`).css("font-weight","bold");  
     }
+  },
+send_message(chatroom_id, body){
+    this.perform("send_message", {chatroom_id: chatroom_id, body: body})
   }
+
+});
+
+$(document).on('turbolinks:load', function() {
+  $("#new_message").submit(function(e){
+    e.preventDefault();
+    let chatroom_id = parseInt($("[data-behavior='messages']").attr("data-chatroom-id"))
+    let message_body = $("#message_body");
+    let body = message_body.val();
+    message_body.val(""); //setting message body empty for new message
+    consumer.subscriptions["subscriptions"][0].send_message(chatroom_id,body)
+  })
+
 });
--- app/javascript/chatroom.js
+++ app/javascript/chatroom.js
@@ -6,4 +6,14 @@ $(document).on('turbolinks:load', function() {
         $(this).submit();
     }
 	});
+//following code is moved to channels/chatrooms_channels.js because App keyword was not working
+	// $("#new_message").submit(function(e){
+	// 	e.preventDefault();
+	// 	chatroom_id = parseInt($("[data-behavior='messages']").attr("data-chatroom-id"))
+	// 	message_body = $("#message_body");
+	// 	body = message_body.val();
+	// 	message_body.val("");	//setting message body empty for new message
+	// 	App.send_message(chatroom_id,body);
+	// })
 });
+
